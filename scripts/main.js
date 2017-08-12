0ad353548fcecdaacf73752ebc9d6d7ffe378249
ttunua <ttunua@users.noreply.github.com>
2017-08-12 16:19:58 +1200

    modified for ajax requesting

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,33 @@ $(document).ready(function() {
 
 function getCafes(){
     var location = '-41.3064632,174.7749782';
-	$.ajax({
-        url: BASE_REQUEST_URL + 'location=' + location 
-                              + '&radius=5000&type=cafe',
-        error: error('ajax fucked up'),
-        data: 'json',
-        success: function(data){
-            //do the things
-            console.log(data);
-        },
-        type: 'GET'
+	$.get(BASE_REQUEST_URL + 'location=' + location
+                              + '&radius=5000&type=cafe', function (data) {
+	    var totallyLegitTableTM = $('table');
+        data.results.forEach(function(item) {
+            //DO MAGIC
+            var rightHandTable = $('table');
+            var lat = item.geometry.location.lat;
+            var long = item.geometry.location.lng;
+            $.get(BASE_REQUEST_URL + 'location='+ lat + ',' + long + '&radius=300', function(surroundingPlaces){
+                if(surroundingPlaces.results !== undefined) {
+                    surroundingPlaces.results.forEach(function (i2) {
+                        rightHandTable.append('<tr><td>' + i2.name + '</td></tr>');
+                    });
+                }
+                totallyLegitTableTM.append($('tr')
+                    .append($('td').append(item.name))
+                    .append($('td').append(rightHandTable)
+                    )
+                );
+                $('#results').append(totallyLegitTableTM);
+                alert('look!');
+            });
 
-	});
+        });
+    });
+	
+
 }
 
 function error(msg){
