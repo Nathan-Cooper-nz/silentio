31060cd404b7cb2aa4167c4df6dfbd3390e06d3c
Nathan Cooper <nathan.cooper.nzl@gmail.com>
2017-08-13 10:25:32 +1200

    Nearby locations now sorted by distance

--- scripts/main.js
+++ scripts/main.js
@@ -10,46 +10,49 @@ $(document).ready(function() {
 function getCafes(){
     var location = '-41.3064632,174.7749782';
 	$.get(BASE_REQUEST_URL + 'location=' + location
-                              + '&radius=5000&type=cafe', function (data) {
-	    var totallyLegitTableTM = $('table');
+                              + '&rankby=distance&type=cafe', function (data) {
+        //array of the ajax requests to later use with a .when statement
+        var ajaxRequests = new Array();
+        //items to be added after the ajax requests are all done
+        var itemsToAdd = new Map();
+
         data.results.forEach(function(item) {
             var name = item.name;
             var lat = item.geometry.location.lat;
             var long = item.geometry.location.lng;
 
+            //a set of unique things around the location
             var locationsNearBy = new Set();
             locationsNearBy.add(name);
-            $.get(BASE_REQUEST_URL + 'location='+ lat + ',' + long + '&radius=100', function(surroundingPlaces){
+            //create an ajax request and add it to the array
+            $.get(BASE_REQUEST_URL + 'location='+ lat + ','
+                            + long + '&radius=100', function(surroundingPlaces){
               var tableRow = document.createElement('tr');
-              // $(tableRow).append($('td').text(name));
               tableRow.insertCell(-1).appendChild(document.createTextNode(name));
+
+              //creates an unorded list of surrounding things
               var listNearby = document.createElement('ul');
+
               var right = tableRow.insertCell(-1).appendChild(listNearby);
+
+              //Add surroundingPlaces
               surroundingPlaces.results.forEach(function (it){
+                //if we havent seen a similar name before
                 if(!locationsNearBy.has(it.name)){
                   var point = document.createElement('li');
                   point.innerHTML=it.name;
                   listNearby.appendChild(point);
                   locationsNearBy.add(it.name);
                 }
-                // listNearby.appendChild(document.createElement('li').appendChild(document.createTextNode(it.name)))
               })
+              // itemsToAdd.set(name, {size: locationsNearBy.size-1, row: tableRow});
               $('#table').append(tableRow);
-                // if(surroundingPlaces.results !== undefined) {
-                //     surroundingPlaces.results.forEach(function (i2) {
-                //         rightHandTable.append('<tr><td>' + i2.name + '</td></tr>');
-                //     });
-                // }
-                // totallyLegitTableTM.append($('tr')
-                //     .append($('td').append(item.name))
-                //     .append($('td').append(rightHandTable)
-                //     )
-                // );
-                // $('#results').append(totallyLegitTableTM);
-                // alert('look!');
             });
-
         });
+        //
+        // $.when.apply($, ajaxRequests).then(function(){
+        //
+        // })
     });
 
 
